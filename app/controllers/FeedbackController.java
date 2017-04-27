001359449fe3936eeaab3cc41ca433b2195876d4
Chandler Severson <seversonc@sou.edu>
2017-04-26 19:06:58 -0700

    Fix placeholder clearing bug for login and register pages.
    Signed-off-by: Chandler Severson <seversonc@sou.edu>

--- app/controllers/FeedbackController.java
+++ app/controllers/FeedbackController.java
@@ -1,15 +1,17 @@
 package controllers;
 
-import play.Play;
-import play.api.libs.mailer.MailerClient;
+import helper.Secured;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.data.FormFactory;
-import play.libs.mailer.Email;
 import play.mvc.Result;
 import views.formdata.FeedbackForm;
+import views.html.feedback;
 
 import javax.inject.Inject;
+
+import static play.mvc.Http.Context.Implicit.ctx;
+import static play.mvc.Results.ok;
 
 /**
  * FeedbackController: Controller to handle the Feedback page.
@@ -24,7 +26,12 @@ public class FeedbackController {
     @Inject FormFactory formFactory;
     Form<FeedbackForm> feedbackForm;
 
-   private static MailController fMail = new MailController();
+    private static MailController fMail = new MailController();
+
+    public Result feedbackPage(){
+        feedbackForm = formFactory.form(FeedbackForm.class);
+        return ok(feedback.render("Share Your Thoughts!", feedbackForm, Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
+    }
 
     public Result sendFeedBack () {
         DynamicForm dynform = Form.form().bindFromRequest();
@@ -32,8 +39,12 @@ public class FeedbackController {
         String email = dynform.get("email");
         String name = dynform.get("name");
         String message = dynform.get("message");
-        int rate;
-        fMail.sendMail("New Feedback From "+ name, "ZAStream Support","support@zastream.com", String.format(feedbackTemplate, name, email, message));
+
+        String rate = dynform.get("star");
+        int rating = Integer.parseInt(rate);
+
+        fMail.sendMail("New Feedback From "+ name, "ZAStream Support","support@zastream.com", String.format(feedbackTemplate, name, email, message, rating));
+        return ok(views.html.feedback.render("Feedback Submitted!", feedbackForm, Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
     }
 
     private static final String feedbackTemplate = "<!DOCTYPE html>\n" +
@@ -50,6 +61,7 @@ public class FeedbackController {
             "       <p>\n" +
             "           %s\n" +
             "       </p>\n" +
+            "<p>Rating from 1 to 5: %d</p>\n"+
             "   </body>\n" +
             "</html>";
 
