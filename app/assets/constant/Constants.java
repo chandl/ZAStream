48005eb251c02f5883c92a6ff317a7667636a880
Chandler Severson <seversonc@sou.edu>
2017-06-04 17:17:41 -0700

    Add Broadcaster Streaming Tutorial Documentation on Homepage
    Signed-off-by: Chandler Severson <seversonc@sou.edu>

--- app/assets/constant/Constants.java
+++ app/assets/constant/Constants.java
@@ -3,15 +3,15 @@ package assets.constant;
 /**
  * Constants: Holds Constant Variables used Throughout the Program.
  *
- * @author Chandler Severson <seversonc@sou.edu>
- * @author Yiwei Zheng <zhengy1@sou.edu>
- * @version 1.0
+ * @author Chandler Severson
+ * @author Yiwei Zheng
+ * @version 2.0
  * @since 1.0
  */
 public class Constants {
     public static final String USERNAME_PLACEHOLDER = "Username";
     public static final String PASSWORD_PLACEHOLDER = "Password";
     public static final String EMAIL_PLACEHOLDER = "Email";
-
-    public static final String RECOVERY_LINK = "https://dev.zastream.com:9000/recover-password/";
+    public static final String HOSTNAME = play.Play.application().configuration().getString("my.HOSTNAME");
+    public static final String RECOVERY_LINK = "https://"+HOSTNAME+"/recover-password/";
 }
